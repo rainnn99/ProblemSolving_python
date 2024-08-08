ca7a9d826859600acf7e7adb10f07d206604af2d
yongtae Park <97329322+rainnn99@users.noreply.github.com>
2024-08-09 00:19:47 +0900

    [level 2] Title: H-Index, Time: 0.22 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,9 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    
+    for h in range(len(citations)):
+        if citations[h] < h + 1:
+            return h
+    
+    else:
+        return len(citations)
